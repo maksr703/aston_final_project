6cbbaed05a7f8b5c79dfc767af26253e537feea2
Maksim Romanov <maksr703@icloud.com>
2026-04-13 14:17:49 +0300

    refactor EmailComparator

--- app/src/main/java/org/example/sorting/EmailComparator.java
+++ app/src/main/java/org/example/sorting/EmailComparator.java
@@ -13,24 +13,15 @@ public class EmailComparator implements Comparator<String> {
         int at1 = o1.indexOf('@');
         int at2 = o2.indexOf('@');
 
-        // Если формат некорректный — сравниваем как строки
         if (at1 == -1 || at2 == -1) {
             return o1.compareTo(o2);
         }
 
-        String local1 = o1.substring(0, at1);
-        String domain1 = o1.substring(at1 + 1);
-
-        String local2 = o2.substring(0, at2);
-        String domain2 = o2.substring(at2 + 1);
-
-        // Сначала сравниваем домены
-        int result = domain1.compareTo(domain2);
+        int result = o1.substring(at1 + 1).compareTo(o2.substring(at2 + 1));
         if (result != 0) {
             return result;
         }
 
-        // Если домены равны — сравниваем local-part
-        return local1.compareTo(local2);
+        return o1.substring(0, at1).compareTo(o2.substring(0, at2));
     }
 }
